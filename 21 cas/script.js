bc16ad8ebf20f7c8d8964a3a60317c6916e54682
UrosAndjelkovic <ukiandjelkovic1222@gmail.com>
2023-10-25 18:34:32 +0200

    commit .commit

---
+++ 21 cas/script.js
@@ -0,0 +1,15 @@
+console.log("Hello Svete");
+// alert("ALOOOOO");
+document.write("dasdasdsadsads");
+console.log(2+2);
+var x
+var a = 5
+var b = 10
+x = a + b
+console.log(x)
+var bool = true
+var undef = undefined
+var text = "string"
+var nula = null
+var broj = x
+console.log(bool, undef, text, nula, broj)
---
+++ 23 cas/script.js
@@ -0,0 +1,4 @@
+const cars = ["bmw", "audi", "vw", "mercedes"]
+const num = [3,4]
+const nums = [1, 2, ...num]
+console.log(nums)
